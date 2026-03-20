cbb9740d4204caef8db1096121f13b478e0f9850
TeoAlex <teoalexsin@gmail.com>
2026-03-20 11:13:23 +0200

    check if slider

--- includes/public/templates/modula-gallery.php
+++ includes/public/templates/modula-gallery.php
@@ -26,8 +26,9 @@
 
 			// Check per gallery & per image if we should show title.
 			$should_hide_title = ( boolval( $data->settings['hide_title'] ) || ( isset( $image['hide_title'] ) && boolval( $image['hide_title'] ) ) );
-			$custom_size       = 'custom' === $data->settings['grid_image_size'] || ( isset( $data->settings['slider_image_crop'] ) && 'custom' === $data->settings['slider_image_size'] );
-			$crop              = boolval( $data->settings['grid_image_crop'] ) || ( isset( $data->settings['slider_image_crop'] ) && boolval( $data->settings['slider_image_crop'] ) );
+			$is_slider         = isset( $data->settings['slider_image_crop'] );
+			$custom_size       = $is_slider ? 'custom' === $data->settings['slider_image_size'] : 'custom' === $data->settings['grid_image_size'];
+			$crop              = $is_slider ? boolval( $data->settings['slider_image_crop'] ) : boolval( $data->settings['grid_image_crop'] );
 
 			// Create array with data in order to send it to image template
 			$item_data = array(
